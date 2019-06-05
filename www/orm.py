53246b2439f55535dc478c2080c07ff80cb1c589
SkyJw_Laptop <fanjw99.com>
2019-06-05 14:16:36 +0800

    完成model元类，部分model类编写

--- www/orm.py
+++ www/orm.py
@@ -79,8 +79,57 @@ class TextField(Field):
 
 
 class ModelMetaclass(type):
-    def __new__(cls, name, bases, attrs):
+    def __new__(cls, name, bases, attrs):#类的属性以字典形式存储在attrs中，key全部为字符串
         if name == 'Model':
             return type.__new__(cls, name, bases, attrs)#如果是Model类，则不需要通过元类来创建
-        tableName = attrs.get('__table__', None) or name#如果没有定义__table__,则使用name作tablename     
+        tableName = attrs.get('__table__', None) or name#如果没有定义__table__,则使用name作tablename
+        logging.info('found model: %s (table : %s)' % (name, tableName))
+        mapping = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items(): #遍历字段属性
+            if isinstance(v, Field):
+                logging.info(' found mapping: %s ==> %s' % (k, v)) 
+                mapping[k] = v #又用一个字典，存储了table中的字段映射关系
+                if v.primary_key:
+                    if primaryKey:
+                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
 
+        if not primaryKey:
+            raise StandardError('primary key not found.')
+        for k in mapping.keys():
+            attrs.pop(k) #将已经保留在mapping中的映射关系，在model子类中去除
+
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields)) #返回一个字符串列表，表中是形如'`name`'的字符串
+
+        attrs['__mapping__'] = mapping #保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey #主键的属性名
+        attrs['__fields__'] = fields #除主键外的所有字段的属性名
+        #使用字符串参数组合出sql语句
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+#class Model(dict, metaclass = ModelMetaclass):
+class Model(dict):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key): # 可以使用Model.key的方式来访问该字典
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
